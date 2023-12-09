3138ab52bb5cd45fda6dd8b8b36b0143eb6b4dca
jakeyjakeyy <jrichca1@gmail.com>
2023-12-09 10:24:27 -0800

    navbuttons animations

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,58 +1,65 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 let contentSelection = ref('home')
 let homeElement: HTMLElement | null = null
 let aboutElement: HTMLElement | null = null
 let projectElement: HTMLElement | null = null
 let arrowUpElement: HTMLElement | null = null
 let arrowDownElement: HTMLElement | null = null
-let autoMotion: Boolean = false
-let timeoutId: number | null = null
 
 const up = () => {
   if (contentSelection.value === 'about') {
-    autoMotion = true
     homeElement?.scrollIntoView({ behavior: 'smooth' })
     contentSelection.value = 'home'
   } else if (contentSelection.value === 'projects') {
-    autoMotion = true
     aboutElement?.scrollIntoView({ behavior: 'smooth' })
     contentSelection.value = 'about'
   }
-  // Prevents the icons from bugging out when scrolling too fast
-  if (timeoutId) clearTimeout(timeoutId)
-  timeoutId = setTimeout(() => {
-    autoMotion = false
-  }, 500)
 }
 const down = () => {
   if (contentSelection.value === 'home') {
-    autoMotion = true
     aboutElement?.scrollIntoView({ behavior: 'smooth' })
     contentSelection.value = 'about'
   } else if (contentSelection.value === 'about') {
-    autoMotion = true
     projectElement?.scrollIntoView({ behavior: 'smooth' })
     contentSelection.value = 'projects'
   }
-  // Prevents the icons from bugging out when scrolling too fast
-  if (timeoutId) clearTimeout(timeoutId)
-  timeoutId = setTimeout(() => {
-    autoMotion = false
-  }, 500)
 }
 
 onscroll = () => {
-  if (homeElement && aboutElement && projectElement && !autoMotion) {
+  if (homeElement && aboutElement && projectElement) {
     const homeRect = homeElement.getBoundingClientRect()
     const aboutRect = aboutElement.getBoundingClientRect()
     const projectRect = projectElement.getBoundingClientRect()
     if (homeRect.top === 0 || homeRect.bottom >= window.innerHeight / 2) {
       contentSelection.value = 'home'
+      arrowUpElement?.classList.remove('arrowUpAppear')
+      arrowUpElement?.classList.remove('arrowUpFromCenter')
+      arrowUpElement?.classList.add('arrowUpDisappear')
+      arrowDownElement?.classList.remove('arrowDownFromCenter')
+      arrowDownElement?.classList.add('arrowDownCentered')
     } else if (aboutRect.top === 0 || aboutRect.bottom >= window.innerHeight / 2) {
       contentSelection.value = 'about'
+      if (arrowUpElement?.classList.contains('arrowUpDisappear')) {
+        arrowUpElement?.classList.remove('arrowUpDisappear')
+        arrowUpElement?.classList.add('arrowUpAppear')
+      } else if (arrowUpElement?.classList.contains('arrowUpCentered')) {
+        arrowUpElement?.classList.remove('arrowUpCentered')
+        arrowUpElement?.classList.add('arrowUpFromCenter')
+      }
+      if (arrowDownElement?.classList.contains('arrowDownCentered')) {
+        arrowDownElement?.classList.remove('arrowDownCentered')
+        arrowDownElement?.classList.add('arrowDownFromCenter')
+      } else if (arrowDownElement?.classList.contains('arrowDownDisappear')) {
+        arrowDownElement?.classList.remove('arrowDownDisappear')
+        arrowDownElement?.classList.add('arrowDownAppear')
+      }
     } else if (projectRect.top === 0 || projectRect.bottom >= window.innerHeight / 2) {
       contentSelection.value = 'projects'
+      arrowUpElement?.classList.remove('arrowUpAppear')
+      arrowUpElement?.classList.add('arrowUpCentered')
+      arrowDownElement?.classList.remove('arrowDownFromCenter')
+      arrowDownElement?.classList.add('arrowDownDisappear')
     }
   }
 
@@ -72,43 +79,130 @@ onload = () => {
   projectElement = document.getElementById('projectContainer')
   arrowUpElement = document.getElementById('arrowUp')
   arrowDownElement = document.getElementById('arrowDown')
+  arrowDownElement?.classList.remove('arrowDownAppear')
+  arrowDownElement?.classList.add('arrowDownCentered')
 }
 </script>
 
 <template>
   <div class="navContainer">
-    <div v-if="contentSelection !== 'home'" id="arrowUp" class="animateUp">
+    <div id="arrowUp" class="arrowUpDisappear">
       <v-icon name="bi-arrow-up-circle" scale="2" @click="up" />
     </div>
-    <div v-if="contentSelection !== 'projects'" class="animateDown" id="arrowDown">
+    <div id="arrowDown" class="arrowDownAppear">
       <v-icon name="bi-arrow-down-circle" scale="2" @click="down" />
     </div>
   </div>
 </template>
 
 <style scoped>
-@keyframes arrowUpAnimation {
+@keyframes arrowUpAppear {
   0% {
-    transform: translateY(30px) scale(0);
+    opacity: 0;
+    transform: translateY(-100px);
   }
   100% {
-    transform: translateY(0px) scale(1);
+    opacity: 1;
+    transform: translateY(-30px);
   }
 }
-@keyframes arrowDownAnimation {
+@keyframes arrowUpDisappear {
   0% {
-    transform: translateY(-30px) scale(0);
+    opacity: 1;
+    transform: translateY(-30px);
   }
   100% {
-    transform: translateY(0px) scale(1);
+    opacity: 0;
+    transform: translateY(-100px);
+  }
+}
+@keyframes arrowUpCentered {
+  0% {
+    transform: translateY(-30px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
+@keyframes arrowUpFromCenter {
+  0% {
+    transform: translateY(0px);
+  }
+  100% {
+    transform: translateY(-30px);
   }
 }
 
-.animateUp {
-  animation: arrowUpAnimation 0.5s ease-in-out;
+@keyframes arrowDownAppear {
+  0% {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(30px);
+  }
 }
-.animateDown {
-  animation: arrowDownAnimation 0.5s ease-in-out;
+
+@keyframes arrowDownCentered {
+  0% {
+    transform: translateY(30px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
+
+@keyframes arrowDownFromCenter {
+  0% {
+    transform: translateY(0px);
+  }
+  100% {
+    transform: translateY(30px);
+  }
+}
+
+@keyframes arrowDownDisappear {
+  0% {
+    opacity: 1;
+    transform: translateY(30px);
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+}
+
+.arrowUpAppear {
+  animation: arrowUpAppear 0.5s ease-in-out forwards;
+}
+
+.arrowUpDisappear {
+  animation: arrowUpDisappear 0.5s ease-in-out forwards;
+}
+
+.arrowUpCentered {
+  animation: arrowUpCentered 0.5s ease-in-out forwards;
+}
+
+.arrowUpFromCenter {
+  animation: arrowUpFromCenter 0.5s ease-in-out forwards;
+}
+
+.arrowDownAppear {
+  animation: arrowDownAppear 0.5s ease-in-out forwards;
+}
+
+.arrowDownCentered {
+  animation: arrowDownCentered 0.5s ease-in-out forwards;
+}
+
+.arrowDownFromCenter {
+  animation: arrowDownFromCenter 0.5s ease-in-out forwards;
+}
+
+.arrowDownDisappear {
+  animation: arrowDownDisappear 0.5s ease-in-out forwards;
 }
 
 .navContainer {
@@ -120,15 +214,19 @@ onload = () => {
   top: 0;
   right: 0;
   height: 100vh;
-  padding: 1rem;
+  width: 60px;
   z-index: 100;
 }
 .hidden {
-  display: none;
+  opacity: 0;
 }
 
 #arrowUp,
 #arrowDown {
   cursor: pointer;
+  position: absolute;
+}
+
+#arrowDown {
 }
 </style>
