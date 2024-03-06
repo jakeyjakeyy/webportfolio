fafa553fd08fd199642a0846b0993f64b7dc653d
jakeyjakeyy <jrichca1@gmail.com>
2024-03-06 05:59:35 -0800

    error fixing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
-    "jakerichards.info",
+    "https://jakerichards.info",
 ]
 
 LOGGING = {
