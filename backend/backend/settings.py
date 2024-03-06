67a47588546fc86069b6fb4881544f26ef953709
jakeyjakeyy <jrichca1@gmail.com>
2024-03-06 09:16:43 -0800

    i love docker and nginx

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["jakerichards.info"]
+ALLOWED_HOSTS = ["jakerichards.info", "146.190.38.15"]
 
 
 # Application definition
