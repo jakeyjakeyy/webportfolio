b6f330ad7436b8f32ab45cafc736264040e4e107
jakeyjakeyy <jrichca1@gmail.com>
2024-06-24 14:23:30 -0700

    undo

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("amFrZWFkbWlu/", admin.site.urls),
     path("api/", include("webportfolio.urls")),
 ]
